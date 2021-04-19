26fe05f9e8ae18e389abbb5e066e74b0052a0672
Vidmasas <vidmantas.burba@shopify.com>
2021-04-19 19:19:56 +0300

    Add axios

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from "vue";
 import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
+import axios from "axios";
+import VueAxios from "vue-axios";
 
 import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 
@@ -9,6 +11,7 @@ import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
 
+Vue.use(VueAxios, axios);
 // Make BootstrapVue available throughout your project
 Vue.use(BootstrapVue);
 // Optionally install the BootstrapVue icon components plugin
